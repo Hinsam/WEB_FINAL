4655ea8ff9af4ebdf59e58d13a4298b273d30952
Songhwa Han <one.pine.pollen@gmail.com>
2023-12-18 00:41:46 +0900

    판매페이지

---
+++ sale_page/test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE test (
+    SaleId INT NOT NULL auto_increment PRIMARY KEY,
+    ItemName varchar(50) not null,
+    UserId varchar(50) not null,
+    ItemDescription text not null,
+    SaleDate varchar(50) not null,
+    SalePrice varchar(50) not null
+);
